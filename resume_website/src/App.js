8525b0f70af75dee475ab0b8b607ce5896bcdc81
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-01-20 13:03:51 -0600

    created resume website

---
+++ resume_website/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+let email = 'edgonzalezhernandez@gmail.com'
+
+function App(){
+  return(
+    <div className="header">
+      <h1>Eduardo Gonzalez Hernandez</h1>
+      <div>{email}</div>
+    </div>
+  )
+}
+
+export default App;
